a420ea1fc55fbd6e03980cb5b3b2cc408ca61778
SpeedOfLightPen <KillerDucks@users.noreply.github.com>
2018-08-22 17:28:50 +0100

    Added Debugging Command
    This is to test the command reloader function.

---
+++ Core/Commands/debug.js
@@ -0,0 +1,9 @@
+// Export the Function
+module.exports.run = async (client, message, userstate, prams) => {
+    client.say("speedoflightpen", `MelissaCrush v1.0.0 --> Symbol DEBUG::RUN Changed`);
+};
+// Export the Meta
+module.exports.help =  {
+    name: "debug",
+    Info: "BASIC INFO"
+};
